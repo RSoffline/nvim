e6bfeb6bfa40b028aa141c7b5acef5c5faf9918a
Ryusei Suda <fudansi.ryusei@gmail.com>
2026-03-19 22:19:14 +0900

    lspの設定を修正

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,14 +1,32 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-vim.lsp.config("pyright", {})
-vim.lsp.config("rust_analyzer", {})
-vim.lsp.config("jsonls", {})
-vim.lsp.config("yamlls", {})
-vim.lsp.config("marksman", {})
+local on_attach = function(_, bufnr)
+  local map = vim.keymap.set
+  local opts = { buffer = bufnr }
 
-vim.lsp.enable({
-  "pyright",
-  "rust_analyzer",
-  "jsonls",
-  "yamlls",
-  "marksman"
-})
+  map("n", "gd", vim.lsp.buf.definition, opts)
+  map("n", "gr", vim.lsp.buf.references, opts)
+  map("n", "K", vim.lsp.buf.hover, opts)
+  map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+end
+
+local function setup(server, config)
+  vim.lsp.config(server, vim.tbl_deep_extend("force", {
+    capabilities = capabilities,
+    on_attach = on_attach,
+  }, config or {}))
+end
+
+-- 必要なものだけ
+servers = {
+    "pyright",
+    "rust_analyzer",
+    "jsonls",
+    "yamlls",
+    "marksman",
+}
+
+for i = 1, #servers do
+    setup(servers[i])
+end
+vim.lsp.enable(servers)
