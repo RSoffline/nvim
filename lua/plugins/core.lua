c3187eb1bce21014a05df350ef9529940f268f8c
Ryusei Suda <fudansi.ryusei@gmail.com>
2026-03-19 20:31:36 +0900

    telescopeを追加

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -87,4 +87,23 @@ return {
  },
 },
 
+-- Telescope
+{
+ "nvim-telescope/telescope.nvim",
+ dependencies = {
+   "nvim-lua/plenary.nvim"
+ },
+ config = function()
+   require("telescope").setup({
+     defaults = {
+       layout_strategy = "horizontal",
+       layout_config = {
+         prompt_position = "top",
+       },
+       sorting_strategy = "ascending",
+     },
+   })
+ end
+},
+
 }
