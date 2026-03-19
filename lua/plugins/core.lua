1b02c9c174f6c00cceb7608b02c88b90ec186adc
Ryusei Suda <fudansi.ryusei@gmail.com>
2026-03-19 22:44:55 +0900

    コメント、自動かっこ、Git差分の拡張機能を追加

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -106,4 +106,27 @@ return {
  end
 },
 
+-- コメント
+{
+  "numToStr/Comment.nvim",
+  config = function()
+    require("Comment").setup()
+  end
+},
+
+-- 自動括弧
+{
+  "windwp/nvim-autopairs",
+  config = function()
+    require("nvim-autopairs").setup()
+  end
+},
+
+-- Git差分（軽量）
+{
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup()
+  end
+},
 }
