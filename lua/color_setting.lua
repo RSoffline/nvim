a48ae342c07b3215952d04e0fe437816aa06beb1
Ryusei Suda <fudansi.ryusei@gmail.com>
2026-04-11 21:48:39 +0900

    UIのスキーマを修正
    ガターの透過
    ステータスバーの透過
    フロートの透過
    非アクティブパネルの透過

--- lua/color_setting.lua
+++ lua/color_setting.lua
@@ -1,11 +1,32 @@
 -- 色設定
+require("tokyonight").setup({
+    transparent = true,
+    styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+    },
+})
+
 -- ターミナルに合わせて背景を透過する
 vim.cmd.colorscheme("tokyonight")
 vim.opt.termguicolors = true
 vim.api.nvim_set_hl(0, 'Normal', {bg='NONE'})
+vim.api.nvim_set_hl(0, 'NormalNC', {bg='NONE'})
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'PmenuSel', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'SpecialKey', { fg = '#969696', bg = 'NONE' }) -- 空白文字の色
 vim.api.nvim_set_hl(0, 'lineNr', { fg = '#9acaee', bg = 'NONE' }) -- 行番号の色
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'FoldColumn', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "TabLineSel", { bg = "NONE" })
 
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -179,8 +179,5 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-        require("tokyonight").setup()
-    end,
 },
 }
